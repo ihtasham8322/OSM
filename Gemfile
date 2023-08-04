19cc5229e184edb6674013b5dd0fd594ddfc356a
Ihtsham Ahmed <ihtasham800@gmail.com>
2023-08-04 16:56:11 +0500

    Multitadency:Install and configure apartment gem

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem "rails", "~> 7.0.6"
 gem "bootsnap", require: false
 gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
+gem 'ros-apartment','~> 2.11',require: 'apartment'
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    before_action :set_tenant
+
+     private
+
+    def set_tenant
+      tenant = Tenant.find_by(subdomain: request.subdomain)
+
+      Apartment::Tenant.switch!(tenant.subdomain) if tenant
+    end
+    
 end
